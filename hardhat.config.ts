8727788504c0e19a5d26093181fa47127066f09c
reindeer002 <lsj000424@gmail.com>
2025-06-22 23:03:58 +0900

    chore: add settings for deploy contract

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,19 @@ import "@nomicfoundation/hardhat-toolbox-viem";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
+  networks: {
+    hardhat: {
+      gas: 10000000,
+      gasPrice: 875000000,
+    },
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL || "",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY || "",
+  },
 };
 
 export default config;
